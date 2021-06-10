98a043958352ba7190ddf38022233b8e04cb100a
faisals345 <2019ugcs015@nitjsr.ac.in>
2021-06-10 15:11:27 +0530

    left view easily imp by recursion nd maxlevel clev

---
+++ BinaryTree/printLeftView.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+//1 2 4 -1  -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+
+node *buildTree()
+{
+    int d;
+    cin >> d;
+
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    node *n = new node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+
+    return n;
+}
+
+int maxLevel=0;
+void leftView(node* root,int currLevel){
+
+    if(root==NULL){
+        return;
+    }
+
+    if(currLevel>maxLevel){
+        cout<<root->data<<" ";
+        maxLevel=currLevel;
+    }
+    leftView(root->left,currLevel+1);
+    leftView(root->right,currLevel+1);
+    return;
+
+
+
+}
+
+
+
+int main()
+{
+
+    node *root = buildTree();
+    //1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+
+    leftView(root,1);
+
+
+
+    return 0;
+}
